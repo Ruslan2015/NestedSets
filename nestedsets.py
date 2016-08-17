f0ccd1898512e7611383526ec7e3a523ff7ecbd1
Руслан <r.dokudaev@yandex.ru>
2016-08-17 17:42:14 +0300

    Первый коммит

---
+++ nestedsets.py
@@ -0,0 +1,19 @@
+# -*-  coding: utf-8 -*-
+
+
+class NestedSets:
+    def __init__(self):
+        self.id = 0
+        self.level = 0
+        self.rk = 0
+        self.lk = 0
+        self.ref = ''
+        self.tree = []
+
+    @staticmethod
+    def showtreetofile():
+        print("Тест")
+
+if __name__ == "__main__":
+    ns = NestedSets()
+    ns.showtreetofile()
