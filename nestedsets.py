60e3c9fe1a806f39b9daede3e217629a6ecdfd93
Руслан <r.dokudaev@yandex.ru>
2016-08-19 16:49:36 +0300

    Заверше первый этап отображения в файл. Сделаны заготовки функций для добавления, удаления узлов и тестирования дерева

--- nestedsets.py
+++ nestedsets.py
@@ -27,38 +27,77 @@ class NestedSets:
 
     def displaytreetofile(self):
         fd = open('files/displaytree', 'w')
-        nodeitem = []
-        nodeitem.append("    |\n")
-        nodeitem.append("  +---+\n")
-        nodeitem.append("  |   |\n")
-        nodeitem.append("+---+---+\n")
-        nodeitem.append("|   |   |\n")
-        nodeitem.append("+---+---+\n")
-        nodeitem.append("|   |   |\n")
-        nodeitem.append("+---+---+\n")
+        # Ключ в словарь записывается так - (rk, lk, level, id)
+        # 0        |
+        # 1    +-------+
+        # 2    |  inf  |
+        # 3    +---+---+
+        # 4    | id|lev|
+        # 5    +---+---+
+        # 6    | lk| rk|
+        # 7    +---+---+
+        # отдельный узел
+        nodeitem = ["    |    ",
+                    "+-------+",
+                    "|       |",
+                    "+---+---+",
+                    "|   |   |",
+                    "+---+---+",
+                    "|   |   |",
+                    "+---+---+"]
+        width = 9  # ширина узла
+        distance = 3  # расстояние между узлами
 
-        for stritem in nodeitem:
-            fd.write(stritem)
+        # тестово отобразим узлы ввсех уровней без учета вложенности
+        # 1 - узнаем максимальное значение уровня в ключах
+        max_lev = 0
+        for key in self.tree.keys():
+            if key[2] > max_lev:
+                max_lev = key[2]
+        # создаем список-страницу
+        page_nodes = []
+        for lev_nodes in range(max_lev):
+            page_nodes.append(lev_nodes)
+        # заполняем каждый уровень
+        levellines = {}  # наборы строк для каждого уровня
+        for (current_level, level) in enumerate(page_nodes):
+            nodelines = []  # набор строк уровня
+            for (count_item_line, itemline) in enumerate(nodeitem):
+                nodeline = ""  # строка в уровне
+                for key in sorted(self.tree.keys()):
+                    if key[2] == current_level + 1:
+                        # здесь должны вставляться цифры
+                        if count_item_line == 4:
+                            itemline = '|{0!s:>3}|{1!s:>3}|'.format(key[3], key[2])
+                        if count_item_line == 6:
+                            itemline = '|{0!s:>3}|{1!s:>3}|'.format(key[0], key[1])
+                        # TODO: Сюда необходимо вставить рассчет интервалов по алгоритму
+                        # 1 - получить всю ветку от родителя и ниже
+                        # 2 - взять максимальный уровень и к-во элементов в нем и умножить на width
+                        nodeline = nodeline + " " * distance + itemline
+                nodelines.append("    " * current_level + nodeline + "\n")
+            levellines[level] = nodelines
+
+        for key in levellines.keys():
+            for lin in levellines[key]:
+                fd.write(lin)
 
         fd.close()
 
+    def add_node(self, parent):
+        # Обновляем ключи узлов, стоящих за родительским узлом
+        # UPDATE my_tree SET left_key = left_key + 2, right_ key = right_ key + 2 WHERE left_key > $right_ key
+        # Обновляем родительскую ветку
+        # UPDATE my_tree SET right_key = right_key + 2 WHERE right_key >= $right_key AND left_key < $right_key
+        # Добавляемновый узел
+        # INSERT INTO my_tree SET left_key = $right_key, right_key = $right_key + 1, level = $level + 1
+        pass
 
-    # def displaytreetofile(self):
-    #     fd = open('files/displaytree', 'w')
-    #     item = """
-    #       +---+
-    #       |   |
-    #     +---+---+
-    #     |   |   |
-    #     +---+---+
-    #     |   |   |
-    #     +---+---+
-    #     """
-    #     for node in sorted(self.tree.keys()):
-    #         fstr = node[2] * '.' + str(node) + ' - ' + self.tree[node] + '\n'
-    #         fd.write(fstr)
-    #     fd.write(item)
-    #     fd.close()
+    def del_node(self, nid):
+        pass
+
+    def test_tree(self):
+        pass
 
 
 if __name__ == "__main__":
